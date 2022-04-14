4a61947eda75745f3d28718522d99904e07f53fe
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-14 10:12:05 -0400

    (c. 10:10 am, 4/14/22 -- commented, on lines 1-2, 'VolunteerProfile')

--- app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
@@ -1,3 +1,6 @@
+/* Richard: put Composables in THIS file, going forward; says when calling 'ProfileImage()', here,
+have "Boolean=true" */
+
 package com.ex.volunteerfinder.view.ui
 
 class VolunteerProfile {

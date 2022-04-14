93586e57f6b8ed47f62121319852b71d6120f20b
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-14 17:53:35 -0400

    'VolunteerProfile' UI changes

--- app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
@@ -3,10 +3,19 @@ package com.ex.volunteerfinder.view.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ex.volunteerfinder.R
 import com.ex.volunteerfinder.view.ui.theme.VolunteerFinderAppTheme
 
 /*  From Richard: put Composables in THIS file, going forward; when calling 'ProfileImage()', here,
@@ -29,5 +38,26 @@ class VolunteerProfile : ComponentActivity() {
         }
 
     }
+}
 
+@Composable
+fun ProfileImage (thumbnail: Boolean = true) {
+
+    Image(painter = painterResource(R.drawable.blankpfp),
+        contentDescription = "Profile Picture",
+        modifier = Modifier.size(
+            if (thumbnail) {
+                40.dp
+            } else {
+                128.dp
+            }
+        ).clip(CircleShape)
+    )
+
+}
+
+@Preview
+@Composable
+fun PreviewProfileImage() {
+    com.ex.volunteerfinder.view.ui.composables.ProfileImage(thumbnail = true)
 }

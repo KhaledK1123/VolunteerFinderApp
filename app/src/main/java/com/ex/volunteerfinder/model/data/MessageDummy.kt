6d8807ad915435aa4504dd041b49d08edcda8baa
Khaled <khali009@cougars.csusm.edu>
2022-04-14 09:51:26 -0700

    Reverted to Micahs Push, and manually added all changes

---
+++ app/src/main/java/com/ex/volunteerfinder/model/data/MessageDummy.kt
@@ -0,0 +1,9 @@
+package com.ex.volunteerfinder.model.data
+
+object MessageDummy {
+    var obj = Message(
+        body = "Hello, my name is Human",
+        sendTime = 1649939435935,
+        users = mutableListOf(UserDummy.obj,UserDummy.obj)
+    )
+}
--- app/src/main/java/com/ex/volunteerfinder/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/ui/VolunteerProfile.kt
@@ -1,4 +1,5 @@
 package com.ex.volunteerfinder.ui
-
+/* Richard: put Composables in THIS file, going forward; says when calling 'ProfileImage()', here,
+have "Boolean=true" */
 class VolunteerProfile {
 }

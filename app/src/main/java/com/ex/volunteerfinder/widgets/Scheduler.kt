0e010edd72f4136df3ac793f7a65a34ceffaa727
Levi21333 <mholt2338@gmail.com>
2022-04-15 17:08:58 -0400

    Added New Widgets for for Create New Event

--- app/src/main/java/com/ex/volunteerfinder/widgets/Scheduler.kt
+++ app/src/main/java/com/ex/volunteerfinder/widgets/Scheduler.kt
@@ -1,12 +1,13 @@
 package com.ex.volunteerfinder.widgets
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.widget.DatePicker
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
@@ -46,4 +47,37 @@ fun dateScheduler():String{
         fontSize = 15.sp,
         textAlign = TextAlign.Center)
     return event
+}
+
+@Composable
+fun timeScheduler():String{
+
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    val hour = calendar[Calendar.HOUR_OF_DAY]
+    val minute = calendar[Calendar.MINUTE]
+
+    var time = remember {
+        mutableStateOf("")
+    }
+
+    val timePickerDialog = TimePickerDialog(
+        context,
+        {_,hour: Int, minute:Int->
+            time.value ="$hour:$minute"
+        },hour,minute,false
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Selected Time: ${time.value}")
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = { timePickerDialog.show() }) {
+            Text(text = "Open Time Picker")
+        }
+    }
+    return time.value
 }

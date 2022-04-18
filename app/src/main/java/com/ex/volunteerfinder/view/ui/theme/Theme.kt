fd54f5cf71f497d24eeb68dfdf3fa1e24ec7d4d6
chinchutreesa <chinchu452@revature.net>
2022-04-18 09:01:51 -0500

    event map

--- app/src/main/java/com/ex/volunteerfinder/view/ui/theme/Theme.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/theme/Theme.kt
@@ -47,3 +47,23 @@ fun VolunteerFinderAppTheme(
         content = content
     )
 }
+
+
+@Composable
+fun GoogleMapComposeV1Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}

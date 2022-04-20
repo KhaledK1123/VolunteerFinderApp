554370e3113968aed2d9b6757f805434625dc0b7
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-20 10:36:38 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt

--- app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
@@ -331,7 +331,7 @@ fun Navigation(navController: NavHostController) {
     }
 }
 
-/*  Starting below, to #374: use, once decide on new layout for a bar (RE: # of volunteered events)
+/*  Starting below, to #374: POSSIBLY use, for a new bar (RE: # of volunteered events)
 @Composable
 fun TableRow(text: String) {
     Text(

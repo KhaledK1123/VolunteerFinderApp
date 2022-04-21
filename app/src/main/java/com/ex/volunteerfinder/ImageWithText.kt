0bad950ce2c5f942366bf7d50ac052b0427ac48b
Khaled <khali009@cougars.csusm.edu>
2022-04-21 10:30:26 -0700

    Some Profile UI

---
+++ app/src/main/java/com/ex/volunteerfinder/ImageWithText.kt
@@ -0,0 +1,7 @@
+package com.ex.volunteerfinder
+import androidx.compose.ui.graphics.painter.Painter
+
+data class ImageWithText(
+    val image: Painter,
+    val text: String
+)
--- app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
@@ -4,21 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -26,10 +32,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ex.volunteerfinder.ImageWithText
 import com.ex.volunteerfinder.MainActivity
 import com.ex.volunteerfinder.R
 import com.ex.volunteerfinder.SignUp
 import com.ex.volunteerfinder.view.MainScreen
+import com.ex.volunteerfinder.view.ui.theme.VolunteerFinderAppTheme
 
 /* Richard's idea: 'Composables' in THIS file, going forward; when calling 'ProfileImage()', here,
 have "Boolean=true" */
@@ -129,7 +137,7 @@ fun PreviewProfileImage() {
 }
 
 //-------------------------------------------------------------------------------------------------
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun ProfileScreenPreview() {
     ProfileScreen()
@@ -194,8 +202,10 @@ fun ProfileScreen() {
                     context.startActivity(Intent(context, SignUp::class.java))
                 })
         }
-    } */
+    }
 }
+*/
+ */
 
 fun TextButton(onClick: () -> Unit) {
 
@@ -306,4 +316,308 @@ fun TableRow(text: String) {
 } */
 
 
+@Composable
+fun ProfileScreen() {
+    VolunteerFinderAppTheme() {
 
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            //verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(colorResource(id = R.color.white))
+                .wrapContentSize(Alignment.TopCenter)
+        ) {
+            //SimpleText2("Donate Item")
+
+            //SubmitButton2()
+
+            ProfileSection()
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    VolunteerProfile()
+}
+
+@Composable
+fun ProfileSection(){
+    Scaffold(
+        topBar = {
+
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.primary,
+                title = {Text("Profile")})
+        }
+    )
+    {
+        Column(Modifier.fillMaxWidth()) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+            ) {
+                RoundImage(
+                    image = painterResource(id = R.drawable.img2),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .weight(3f)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                StatSection(modifier = Modifier.weight(7f))
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            ProfileDescription(
+                displayName = "John Adams",
+                username = "AdamsApple74",
+                email = "JohnnyBoy74@gmail.com",
+                address = "47129 Cabron Ave, Winchester, CA, 92596",
+                donationCount = 18
+            )
+            Column(
+
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(bottom = 75.dp),
+                Arrangement.Top,
+                Alignment.CenterHorizontally
+            ) {
+                belowProfile()
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                Arrangement.Bottom,
+                Alignment.Start
+
+            ) {
+                val context = LocalContext.current
+                TextButton(
+                    onClick = {
+                        //context.startActivity(Intent(context, EditProfileView::class.java))
+                    },
+                ) {
+                    Text(
+                        "Edit Profile",
+                        style = MaterialTheme.typography.button,
+                        color = Color(0xFF673AB7)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+@Composable
+fun StatSection(modifier: Modifier = Modifier){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "18", text = "Donations")
+        ProfileStat(numberText = "16", text = "Items Received")
+        ProfileStat(numberText = "523", text = "Flair")
+    }
+}
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            style = MaterialTheme.typography.body1,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
+    }
+}
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    username: String,
+    email: String,
+    address: String,
+    donationCount: Int
+){
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = "Name: $displayName",
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = "Username: $username",
+            style = MaterialTheme.typography.body1,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = "Email: $email",
+            style = MaterialTheme.typography.body1,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = "Address: $address",
+            style = MaterialTheme.typography.body1,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = buildAnnotatedString {
+                append("Donated $donationCount times!")
+            },
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun belowProfile()
+{
+    var selectedTabIntex by remember {
+        mutableStateOf(0)
+    }
+    PostTabView(
+        imageWithText = listOf(
+            ImageWithText(
+                image = painterResource(id = R.drawable.ic_grid),
+                text = "Posts"
+            ),
+            ImageWithText(
+                image = painterResource(id = R.drawable.ic_reels),
+                text = "Reels"
+            ),
+            ImageWithText(
+                image = painterResource(id = R.drawable.ic_igtv),
+                text = "IGTV"
+            ),
+            ImageWithText(
+                image = painterResource(id = R.drawable.ic_profile),
+                text = "Profile"
+            ),
+        )
+    ){
+        selectedTabIntex = it
+    }
+    when(selectedTabIntex){
+        0 -> PostSection(
+            posts = listOf(
+                painterResource(id = R.drawable.iphone),
+                painterResource(id = R.drawable.mercedes)
+
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun PostTabView(
+    modifier: Modifier = Modifier,
+    imageWithText: List<ImageWithText>,
+    onTabSelected: (selectedIndex: Int) -> Unit
+){
+    var selectedTabIndex by remember {
+        mutableStateOf(0)
+    }
+    val inactiveColor = Color(0xFF777777)
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        backgroundColor = Color.Transparent,
+        contentColor = Color.Black,
+        modifier = modifier
+    ) {
+        imageWithText.forEachIndexed { index, item ->
+            Tab(
+                selected = selectedTabIndex == index,
+                selectedContentColor = Color.Black,
+                unselectedContentColor = inactiveColor,
+                onClick = {
+                    selectedTabIndex = index
+                    onTabSelected(index)
+                }
+            ) {
+                Icon(
+                    painter = item.image,
+                    contentDescription = item.text,
+                    tint = if (selectedTabIndex == index) Color.Black else inactiveColor,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(20.dp)
+                )
+            }
+        }
+    }
+}
+@ExperimentalFoundationApi
+@Composable
+fun PostSection(
+    posts: List<Painter>,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(3),
+        modifier = modifier
+            .scale(1.01f)
+    ) {
+        items(posts.size) {
+            Image(
+                painter = posts[it],
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .border(
+                        width = 1.dp,
+                        color = Color.White
+                    )
+            )
+        }
+    }
+}

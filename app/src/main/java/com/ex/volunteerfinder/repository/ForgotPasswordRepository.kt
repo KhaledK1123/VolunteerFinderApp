5e1bf438cf17f0035036f7569a0fa7e7dd0f7622
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-21 18:05:18 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt

--- app/src/main/java/com/ex/volunteerfinder/repository/ForgotPasswordRepository.kt
+++ app/src/main/java/com/ex/volunteerfinder/repository/ForgotPasswordRepository.kt
@@ -2,7 +2,7 @@ package com.ex.volunteerfinder.repository
 
 import com.ex.volunteerfinder.view.ui.VolunteerProfile
 
-data class ForgotPasswordRepository(var volunteerProfile: VolunteerProfile) {
+data class ForgotPasswordRepository(val volunteerProfileId: VolunteerProfile) {
 
 
 }

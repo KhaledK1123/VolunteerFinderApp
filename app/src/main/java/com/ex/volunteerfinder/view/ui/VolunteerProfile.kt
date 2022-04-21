16672b4b63f7ab7c255615608fabb78cc4c43385
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-21 11:05:43 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt

--- app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
+++ app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt
@@ -62,20 +62,15 @@ class VolunteerProfile : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VolunteerFinderAppTheme {
+            MainScreen()
+        }
+            /* VolunteerFinderAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                ) {
-                    MainScreen()
-                }
-
+                ) */
             }
-
-        }
-
-    }
 }
 
 @Composable

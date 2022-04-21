f9aa91577f4180c544fc6f68a8835645f216d2db
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-04-21 18:25:15 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/ex/volunteerfinder/view/ui/VolunteerProfile.kt

--- app/src/main/java/com/ex/volunteerfinder/repository/ForgotPasswordRepository.kt
+++ app/src/main/java/com/ex/volunteerfinder/repository/ForgotPasswordRepository.kt
@@ -1,8 +1,20 @@
 package com.ex.volunteerfinder.repository
 
-import com.ex.volunteerfinder.view.ui.VolunteerProfile
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.ex.volunteerfinder.model.data.AppDatabase
+import com.ex.volunteerfinder.model.data.MyEvent
 
-data class ForgotPasswordRepository(var volunteerProfileId: VolunteerProfile) {
-
+class ForgotPasswordRepository(application: Application) {
+/*
+    lateinit var forgotPasswordDao: ForgotPasswordDao
+    init {
+        var database = AppDatabase.getDatabase(application)
+        forgotPasswordDao = database.forgotPasswordDao()
+    }
+    val readAllForgotPasswords: LiveData<List<MyForgotPassword>>
+        = forgotPasswordDao.fecthAllForgotPasswords
+Commented out, until data class is created */
 
 }
+
